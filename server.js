ed25cf37de2e729cb3663d286424ef7de6149584
Ronald Castillo <ronaldc@labreferencia.com>
2022-04-21 21:40:49 -0400

    resolve route

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const port = process.env.PORT || 8080
 const app = express()
 
 app.use(express.static(__dirname + "/dist/"))
-app.get(/.*/, () => {
-     res.sendfile(__dirname + "/dist/index.html")
+app.get(/.*/, (_, res) => {
+     res.sendFile(__dirname + "/dist/index.html")
 })
 
 app.listen(port)
